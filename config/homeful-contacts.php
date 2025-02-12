6c3fd5a149e4e49158f5f74ba069d34f147dd999
jn-devops <devops@joy-nostalg.com>
2025-02-12 16:48:50 +0800

    fix sms keyword

--- config/homeful-contacts.php
+++ config/homeful-contacts.php
@@ -14,7 +14,7 @@ return [
     'channels' => [
         'default' => array_filter(explode(',', env('DEFAULT_CHANNELS', 'database'))),
         'allowed' => array_filter(explode(',', env('ALLOWED_CHANNELS', 'database,slack'))),
-        SendContactReferenceCodeNotification::class => array_filter(explode(',', env('SEND_CONTACT_REFERENCE_CODE', 'mail,sms'))),
-        SendLoginMagicLinkNotification::class => array_filter(explode(',', env('SEND_LOGIN_MAGIC_LINK', 'mail,sms'))),
+        SendContactReferenceCodeNotification::class => array_filter(explode(',', env('SEND_CONTACT_REFERENCE_CODE', 'mail,engage_spark'))),
+        SendLoginMagicLinkNotification::class => array_filter(explode(',', env('SEND_LOGIN_MAGIC_LINK', 'mail,engage_spark'))),
     ],
 ];
