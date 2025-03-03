678932e022567b654375c3451b55afdf61bbd333
RDC15 <34361260+renzoDC15@users.noreply.github.com>
2025-03-03 17:54:20 +0800

    update media-library config media_model to Homeful\Contacts\Models\Media::class

--- config/media-library.php
+++ config/media-library.php
@@ -34,7 +34,8 @@ return [
     /*
      * The fully qualified class name of the media model.
      */
-    'media_model' => Spatie\MediaLibrary\MediaCollections\Models\Media::class,
+//    'media_model' => Spatie\MediaLibrary\MediaCollections\Models\Media::class,
+    'media_model' =>Homeful\Contacts\Models\Media::class,
 
     /*
      * When enabled, media collections will be serialised using the default
