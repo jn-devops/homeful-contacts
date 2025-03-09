6a11d78deabd96d89783312ac2f9f63fc5397ebd
Anais Santos <anaiszx@Anaiss-MacBook-Air.local>
2025-03-09 16:39:58 +0800

    Install horizon

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,7 +2,8 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    \Homeful\References\ReferencesServiceProvider::class,
-    \Spatie\SchemalessAttributes\SchemalessAttributesServiceProvider::class,
-    \Homeful\Contacts\ContactsServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
+    Homeful\Contacts\ContactsServiceProvider::class,
+    Homeful\References\ReferencesServiceProvider::class,
+    Spatie\SchemalessAttributes\SchemalessAttributesServiceProvider::class,
 ];
