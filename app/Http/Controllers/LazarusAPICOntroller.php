9752934bcbd1d1888ded8634e54810e3956dd71b
Christian Quelonio <cequelonio@joy-nostalg.com>
2025-03-15 19:39:02 +0800

    fix auth in api

--- app/Http/Controllers/LazarusAPICOntroller.php
+++ app/Http/Controllers/LazarusAPICOntroller.php
@@ -15,20 +15,33 @@ use Illuminate\Validation\ValidationException;
 class LazarusAPICOntroller extends Controller
 {
     public function login(Request $request){
-        $request->validate([
-            'email' => 'required|email',
-            'password' => 'required',
-        ]);
-    
-        $user = User::where('email', $request->email)->first();
-    
-        if (!$user || !Hash::check($request->password, $user->password)) {
-            return response()->json(['message' => 'Invalid credentials'], 401);
+        try {
+            $request->validate([
+                'email' => 'required|email',
+                'password' => 'required',
+            ]);
+        
+            $user = User::where('email', $request->email)->first();
+            if($user instanceof User){
+                if (!$user || !Hash::check($request->password, $user->password)) {
+                    return response()->json(['message' => 'Invalid credentials'], 401);
+                }
+            
+                return response()->json([
+                    'token' => $user->createToken('api-token')->plainTextToken,
+                ]);
+
+            }else{
+                return response()->json(['message' => 'Invalid credentials'], 401);
+            }
+
+        }catch (ValidationException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Validation failed',
+                'errors' => $e->errors()
+            ], 422);
         }
-    
-        return response()->json([
-            'token' => $user->createToken('api-token')->plainTextToken,
-        ]);
     }
 
     public function getContactByID($id){
