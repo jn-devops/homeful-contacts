a3ee22bdff4aada8c3d908d98fdaa5c7734af6e4
Christian Quelonio <cequelonio@joy-nostalg.com>
2025-03-19 11:13:07 +0800

    modify consultation redirect

--- app/Http/Controllers/RedirectControllers.php
+++ app/Http/Controllers/RedirectControllers.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use FrittenKeeZ\Vouchers\Models\VoucherEntity;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class RedirectControllers extends Controller
 {
-    public function redirect_to_consult(){
-        return Inertia::location(config('contract.contract_url').'consult/create?contact_reference_code=');
+    public function redirect_to_consult(Request $request){
+        $homeful_id = VoucherEntity::where('entity_id', $request->user()->contact_id)->first()->voucher->code ?? '';
+        return Inertia::location(config('contract.contract_url').'consult/create?contact_reference_code='.$homeful_id);
     }
 }
