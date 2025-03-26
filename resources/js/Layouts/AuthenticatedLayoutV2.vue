587ed6e7a9063a6afc0412e966385be4b3e5887f
Christian Quelonio <cequelonio@joy-nostalg.com>
2025-03-26 14:55:39 +0800

    revert logout

--- resources/js/Layouts/AuthenticatedLayoutV2.vue
+++ resources/js/Layouts/AuthenticatedLayoutV2.vue
@@ -43,16 +43,18 @@ const emit = defineEmits(['trigger-exit'])
         <div class="mx-auto max-w-7xl lg:px-8">
             <div class="fixed top-0 left-0 w-full bg-gray-100 z-10">
                 <div class="px-4 py-5 text-sm border-b-2">
-                    <button @click="emit('trigger-exit')">
+                    <!-- <button @click="emit('trigger-exit')"> -->
+                    <Link href="/dashboard">
                         <div class="flex flex-row items-center gap-3 cursor-pointer">
                             <div>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="4" stroke="currentColor" class="size-4 text-[#006FFD]">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
                                 </svg>
                             </div>
-                            <h4 class="font-bold">Logout</h4>
+                            <h4 class="font-bold">Home Loan Consultation</h4>
                         </div>
-                    </button>
+                    </Link>
+                    <!-- </button> -->
                 </div>
                 <div class="">
                     <BreadCrumbs :pages="breadcrumbPages" />
