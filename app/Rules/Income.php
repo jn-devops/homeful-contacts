80eb66513870d732ed2fff5e60b11e02e928f4df
Christian Quelonio <cequelonio@joy-nostalg.com>
2025-06-26 09:21:57 +0800

    temporarily make the minumum income to 9999

--- app/Rules/Income.php
+++ app/Rules/Income.php
@@ -15,7 +15,8 @@ class Income implements ValidationRule
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
         $value = (float) $value;
-        $minimum_income = config('homeful-contacts.minimum_income');
+        // $minimum_income = config('homeful-contacts.minimum_income');
+        $minimum_income = 9999;
         if ($value < 0)
             $fail('The :attribute must not be negative.');
         elseif ($value > 0 && $value < $minimum_income)
